e6c589767c393235395dc30a22df6935539cf9f3
Iván Litteri <ilitteri@fi.uba.ar>
2023-07-06 17:52:31 -0300

    Improve call
    Now you can send the raw data and the output type of the function

--- src/commands/call.rs
+++ src/commands/call.rs
@@ -1,6 +1,10 @@
 use crate::cli::ZKSyncWeb3Config;
 use clap::Args;
+use zksync_web3_rs::abi::{decode, ParamType, Tokenize};
+use zksync_web3_rs::providers::Middleware;
 use zksync_web3_rs::signers::LocalWallet;
+use zksync_web3_rs::types::transaction::eip2718::TypedTransaction;
+use zksync_web3_rs::types::{Bytes, Eip1559TransactionRequest};
 use zksync_web3_rs::zks_provider::ZKSProvider;
 use zksync_web3_rs::{providers::Provider, types::Address};
 
@@ -13,6 +17,10 @@ pub(crate) struct Call {
     pub function: String,
     #[clap(short, long, num_args(1..), name = "FUNCTION_ARGS")]
     pub args: Option<Vec<String>>,
+    #[clap(long, name = "DATA")]
+    pub data: Option<Bytes>,
+    #[clap(long, num_args(1..), requires = "data", name = "OUTPUT_TYPES")]
+    pub output_types: Option<Vec<String>>,
     #[clap(short, long, name = "PRIVATE_KEY")]
     pub private_key: LocalWallet,
 }
@@ -32,8 +40,28 @@ pub(crate) async fn run(args: Call, config: ZKSyncWeb3Config) -> eyre::Result<()
         &args.function
     };
 
-    // TODO: Figure out how to parse the args correctly.
-    let output = ZKSProvider::call(&provider, args.contract, function_signature, args.args).await?;
+    let output = if let Some(data) = args.data {
+        let request = Eip1559TransactionRequest::new()
+            .to(args.contract)
+            .data(data);
+        let transaction: TypedTransaction = request.into();
+        let encoded_output = Middleware::call(&provider, &transaction, None).await?;
+        let decoded_output = if let Some(output_types) = args.output_types {
+            let parsed_param_types: Vec<ParamType> = output_types
+                .iter()
+                .map(|output_type| match output_type.as_str() {
+                    "uint256" => ParamType::Uint(256),
+                    _ => todo!(),
+                })
+                .collect();
+            decode(&parsed_param_types, &encoded_output)?
+        } else {
+            encoded_output.into_tokens()
+        };
+        decoded_output
+    } else {
+        ZKSProvider::call(&provider, args.contract, function_signature, args.args).await?
+    };
     log::info!("{output:?}");
     Ok(())
 }
