c9d1051caf13ac85341596d667e4a72b12ec5ce7
Iván Litteri <ilitteri@fi.uba.ar>
2023-10-22 00:01:27 -0300

    Handle deposit on other accounts

--- src/commands/deposit.rs
+++ src/commands/deposit.rs
@@ -4,7 +4,7 @@ use eyre::ContextCompat;
 use zksync_web3_rs::{
     providers::{Middleware, Provider},
     signers::{LocalWallet, Signer},
-    types::U256,
+    types::{Address, U256},
     utils::parse_units,
     zks_wallet::DepositRequest,
     ZKSWallet,
@@ -15,7 +15,9 @@ pub struct Args {
     #[clap(short, long, name = "AMOUNT_TO_DEPOSIT_IN_ETHER")]
     amount: U256,
     #[clap(short, long, name = "SENDER_PRIVATE_KEY")]
-    on: LocalWallet,
+    from: LocalWallet,
+    #[clap(short, long, name = "RECEIVER_ADDRESS")]
+    to: Option<Address>,
     #[clap(short, long, name = "CHAIN_ID")]
     chain_id: u16,
 }
@@ -34,7 +36,7 @@ pub(crate) async fn run(args: Args, config: ZKSyncConfig) -> eyre::Result<()> {
         host = config.host,
         port = config.l2_port
     ))?;
-    let wallet = args.on.with_chain_id(args.chain_id);
+    let wallet = args.from.with_chain_id(args.chain_id);
     let zk_wallet = ZKSWallet::new(
         wallet,
         None,
@@ -43,7 +45,11 @@ pub(crate) async fn run(args: Args, config: ZKSyncConfig) -> eyre::Result<()> {
     )?;
 
     let l1_balance_before = zk_wallet.eth_balance().await?;
-    let l2_balance_before = zk_wallet.era_balance().await?;
+    let l2_balance_before = if let Some(to) = args.to {
+        l2_provider.get_balance(to, None).await?
+    } else {
+        zk_wallet.era_balance().await?
+    };
     println!("L1 balance before: {l1_balance_before}");
     println!("L2 balance before: {l2_balance_before}");
 
@@ -58,7 +64,11 @@ pub(crate) async fn run(args: Args, config: ZKSyncConfig) -> eyre::Result<()> {
         .await?;
 
     let l1_balance_after = zk_wallet.eth_balance().await?;
-    let l2_balance_after = zk_wallet.era_balance().await?;
+    let l2_balance_after = if let Some(to) = args.to {
+        l2_provider.get_balance(to, None).await?
+    } else {
+        zk_wallet.era_balance().await?
+    };
     log::info!("L1 balance after: {l1_balance_after}");
     log::info!("L2 balance after: {l2_balance_after}");
     Ok(())
