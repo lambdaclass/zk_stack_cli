0e3f0742823ab7004390f5264c33003e258b8d9c
Iván Litteri <ilitteri@fi.uba.ar>
2023-11-09 13:48:15 -0300

    Add logger filters

--- src/main.rs
+++ src/main.rs
@@ -4,6 +4,8 @@ use zksync_era_cli::cli;
 async fn main() {
     env_logger::builder()
         .filter_module("reqwest::connect", log::LevelFilter::Off)
+        .filter_module("rustls::client", log::LevelFilter::Off)
+        .filter_module("rustls::common_state", log::LevelFilter::Off)
         .filter_level(log::LevelFilter::Debug)
         .init();
 
