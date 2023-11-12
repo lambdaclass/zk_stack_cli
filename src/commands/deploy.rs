e5806066f3f50671b43bc9b9f1451c953fa22d98
Iván Litteri <ilitteri@fi.uba.ar>
2023-11-12 13:07:14 -0300

    Update `deploy` cmd

--- src/commands/deploy.rs
+++ src/commands/deploy.rs
@@ -1,10 +1,9 @@
 use crate::cli::ZKSyncConfig;
 use crate::commands::compile;
+use crate::commands::compile::output::ZKSArtifact;
 use clap::Args as ClapArgs;
 use eyre::ContextCompat;
-use zksync_web3_rs::prelude::abi::Token;
 use zksync_web3_rs::signers::LocalWallet;
-use zksync_web3_rs::types::Bytes;
 use zksync_web3_rs::zks_wallet::DeployRequest;
 use zksync_web3_rs::ZKSWallet;
 use zksync_web3_rs::{providers::Provider, signers::Signer};
@@ -17,22 +16,27 @@ pub(crate) struct Args {
         long,
         name = "CONTRACT PATH",
         requires = "contract_name",
-        conflicts_with = "bytecode"
+        conflicts_with = "contract_artifact"
     )]
     pub contract: Option<String>,
     #[clap(
         long,
         name = "CONTRACT NAME",
         requires = "contract",
-        conflicts_with = "bytecode"
+        conflicts_with = "contract_artifact"
     )]
     pub contract_name: Option<String>,
+    #[clap(
+        long,
+        name = "CONTRACT NAME",
+        requires = "contract",
+        conflicts_with = "contract, contract_name"
+    )]
+    pub contract_artifact: Option<String>,
     #[clap(long, num_args(1..), name = "CONSTRUCTOR_ARGS")]
     constructor_args: Vec<String>,
     #[clap(short, long, name = "PRIVATE KEY")]
     pub private_key: LocalWallet,
-    #[clap(long, name = "CONTRACT BYTECODE")]
-    pub bytecode: Option<Bytes>,
     #[clap(long, name = "CHAIN_ID")]
     pub chain_id: u16,
 }
@@ -45,15 +49,11 @@ pub(crate) async fn run(args: Args, config: ZKSyncConfig) -> eyre::Result<()> {
     ))?;
     let wallet = args.private_key.with_chain_id(args.chain_id);
     let zk_wallet = ZKSWallet::new(wallet, None, Some(era_provider.clone()), None)?;
-    let contract_address = if let Some(bytecode) = args.bytecode {
-        zk_wallet
-            .deploy_from_bytecode(&bytecode, None, None::<Token>)
-            .await?
-    } else if let Some(contract_path) = args.contract.clone() {
+    let contract_address = if let Some(contract_path) = args.contract {
         let artifact = compile::compiler::compile(
             &args.project_root,
             &contract_path,
-            &args.contract_name.context("no contract name provided")?,
+            &args.contract_name.context("Contract name is required")?,
             compile::compiler::Compiler::ZKSolc,
         )?;
 
@@ -66,10 +66,21 @@ pub(crate) async fn run(args: Args, config: ZKSyncConfig) -> eyre::Result<()> {
             args.constructor_args,
         );
 
-        // TODO(Ivan): Wait until deploy does not compile anymore.
+        zk_wallet.deploy(&deploy_request).await?
+    } else if let Some(artifact_path) = args.contract_artifact {
+        let artifact: ZKSArtifact = serde_json::from_slice(&std::fs::read(artifact_path)?)?;
+        let compiled_bytecode = artifact.bin.context("Contract bytecode is missing")?;
+        let compiled_abi = artifact.abi.context("Contract ABI is missing")?;
+
+        let deploy_request = DeployRequest::with(
+            compiled_abi,
+            compiled_bytecode.to_vec(),
+            args.constructor_args,
+        );
+
         zk_wallet.deploy(&deploy_request).await?
     } else {
-        panic!("no bytecode or contract path provided")
+        return Err(eyre::eyre!("Contract path or artifact path is required"));
     };
     log::info!("{contract_address:#?}");
     Ok(())
