10a4ec5a214be2a8608ea4741742f03bf81d0586
Ivan Litteri <67517699+ilitteri@users.noreply.github.com>
2023-11-21 05:45:40 -0300

    Fix `call` & `send` argument overlapping (#31)
    * Fix `call` & `send` argument overlapping
    
    * Fix clippy and fmt
    
    * Add logger filters (#32)

--- src/commands/call.rs
+++ src/commands/call.rs
@@ -15,9 +15,9 @@ pub const ERA_IN_MEMORY_NODE_CHAIN_ID: u16 = 260;
 pub(crate) struct Args {
     #[clap(short, long, name = "CONTRACT_ADDRESS")]
     pub contract: Address,
-    #[clap(short, long, name = "FUNCTION_SIGNATURE")]
-    pub function: String,
-    #[clap(short, long, num_args(1..), name = "FUNCTION_ARGS")]
+    #[clap(short, long, name = "FUNCTION_SIGNATURE", conflicts_with = "data")]
+    pub function: Option<String>,
+    #[clap(short, long, num_args(1..), name = "FUNCTION_ARGS", conflicts_with = "data")]
     pub args: Option<Vec<String>>,
     #[clap(long, name = "DATA")]
     pub data: Option<Bytes>,
@@ -34,53 +34,60 @@ pub(crate) async fn run(args: Args, config: ZKSyncConfig) -> eyre::Result<()> {
         Provider::try_from(config.host.clone())?
     };
 
-    // Note: CLI syntactic sugar need to be handle in the run() function.
-    // If more sugar cases are needed, we should switch to a match statement.
-    let function_signature = if args.function.is_empty() {
-        "function()"
-    } else {
-        &args.function
-    };
-
     let mut request = Eip1559TransactionRequest::new()
         .to(args.contract)
         .chain_id(args.chain_id);
 
+    let func: String;
     if let Some(data) = args.data {
         request = request.data(data);
+    } else {
+        // Note: CLI syntactic sugar need to be handle in the run() function.
+        // If more sugar cases are needed, we should switch to a match statement.
+        let function_signature = if args
+            .function
+            .clone()
+            .context("No function signature provided")?
+            .is_empty()
+        {
+            "function()"
+        } else {
+            func = args.function.context("No function signature provided")?;
+            &func
+        };
+
+        let function = if args.contract == zks_utils::ECADD_PRECOMPILE_ADDRESS {
+            zks_utils::ec_add_function()
+        } else if args.contract == zks_utils::ECMUL_PRECOMPILE_ADDRESS {
+            zks_utils::ec_mul_function()
+        } else if args.contract == zks_utils::MODEXP_PRECOMPILE_ADDRESS {
+            zks_utils::mod_exp_function()
+        } else {
+            HumanReadableParser::parse_function(function_signature)?
+        };
+
+        let function_args = if let Some(function_args) = args.args {
+            function.decode_input(&zks_utils::encode_args(&function, &function_args)?)?
+        } else {
+            vec![]
+        };
+
+        let data = match (
+            !function_args.is_empty(),
+            zks_utils::is_precompile(args.contract),
+        ) {
+            // The contract to call is a precompile with arguments.
+            (true, true) => encode(&function_args),
+            // The contract to call is a regular contract with arguments.
+            (true, false) => function.encode_input(&function_args)?,
+            // The contract to call is a precompile without arguments.
+            (false, true) => Default::default(),
+            // The contract to call is a regular contract without arguments.
+            (false, false) => function.short_signature().into(),
+        };
+
+        request = request.data(data);
     }
-
-    let function = if args.contract == zks_utils::ECADD_PRECOMPILE_ADDRESS {
-        zks_utils::ec_add_function()
-    } else if args.contract == zks_utils::ECMUL_PRECOMPILE_ADDRESS {
-        zks_utils::ec_mul_function()
-    } else if args.contract == zks_utils::MODEXP_PRECOMPILE_ADDRESS {
-        zks_utils::mod_exp_function()
-    } else {
-        HumanReadableParser::parse_function(function_signature)?
-    };
-
-    let function_args = if let Some(function_args) = args.args {
-        function.decode_input(&zks_utils::encode_args(&function, &function_args)?)?
-    } else {
-        vec![]
-    };
-
-    let data = match (
-        !function_args.is_empty(),
-        zks_utils::is_precompile(args.contract),
-    ) {
-        // The contract to call is a precompile with arguments.
-        (true, true) => encode(&function_args),
-        // The contract to call is a regular contract with arguments.
-        (true, false) => function.encode_input(&function_args)?,
-        // The contract to call is a precompile without arguments.
-        (false, true) => Default::default(),
-        // The contract to call is a regular contract without arguments.
-        (false, false) => function.short_signature().into(),
-    };
-
-    request = request.data(data);
 
     let transaction: TypedTransaction = request.into();
 
