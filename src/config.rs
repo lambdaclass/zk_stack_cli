472076206dc5e1eb411d7a0253a73fac7ed16cb2
ilitteri <ilitteri@fi.uba.ar>
2024-08-13 09:19:45 -0300

    Update config

--- src/config.rs
+++ src/config.rs
@@ -1,11 +1,24 @@
 use serde::Deserialize;
+use zksync_ethers_rs::types::Address;
 
 #[derive(Deserialize)]
 pub struct ZKSyncConfig {
-    pub l1_rpc_url: String,
+    pub network: NetworkConfig,
+    pub wallet: Option<WalletConfig>,
+}
+
+#[derive(Deserialize)]
+pub struct NetworkConfig {
+    pub l1_rpc_url: Option<String>,
     pub l1_explorer_url: Option<String>,
     pub l2_rpc_url: String,
     pub l2_explorer_url: Option<String>,
+}
+
+#[derive(Deserialize)]
+pub struct WalletConfig {
+    pub address: Address,
+    pub private_key: String,
 }
 
 pub fn config_path() -> String {
