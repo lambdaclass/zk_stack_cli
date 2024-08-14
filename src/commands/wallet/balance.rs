28dab9fcbab9fcce553c039d28bb7acf71babaea
ilitteri <ilitteri@fi.uba.ar>
2024-08-13 21:10:37 -0300

    Implement `wallet balance` method
    - Used with no args will get you the base token balance on L2
    - With the `token` arg you'll get the account's balance on that token
    - With the `l1` flag you'll get either the base token or passed token balance on L1

--- src/commands/wallet/balance.rs
+++ src/commands/wallet/balance.rs
@@ -12,19 +12,30 @@ use zksync_ethers_rs::{
 pub(crate) struct Args {
     #[clap(long = "token")]
     pub token_address: Option<Address>,
+    #[clap(long = "l1", required = false)]
+    pub l1: bool,
 }
 
 pub(crate) async fn run(args: Args, cfg: ZKSyncConfig) -> eyre::Result<()> {
-    let provider = Provider::try_from(cfg.network.l2_rpc_url)?;
+    let provider = if args.l1 {
+        Provider::try_from(
+            cfg.network
+                .l1_rpc_url
+                .context("L1 RPC URL missing in config")?,
+        )?
+    } else {
+        Provider::try_from(cfg.network.l2_rpc_url)?
+    };
     let wallet_address = cfg.wallet.context("Wallet config missing")?.address;
+    let network = args.l1.then_some("L1").unwrap_or("L2");
     if let Some(token_address) = args.token_address {
         let erc20 = ERC20::new(token_address, Arc::new(provider));
         let balance = erc20.balance_of(wallet_address).await?;
         let token_symbol = erc20.symbol().await?;
-        println!("Balance: {balance} {token_symbol}");
+        println!("[{network}] Balance: {balance} {token_symbol}");
     } else {
         let balance = provider.get_balance(wallet_address, None).await?;
-        println!("Base Token Balance: {balance}");
+        println!("[{network}] Base Token Balance: {balance}");
     }
     Ok(())
 }
