761044d5031fb50353f3f1faa33bfb59e0d4b4f8
fborello-lambda <federicoborello@lambdaclass.com>
2024-08-14 18:29:57 -0300

    refactor(balance): make it more straightforward

--- src/commands/wallet/balance.rs
+++ src/commands/wallet/balance.rs
@@ -5,7 +5,7 @@ use std::sync::Arc;
 use zksync_ethers_rs::{
     contracts::{erc20::ERC20, l2_shared_bridge::get_l2_token_from_l1_address},
     core::utils::{format_ether, format_units},
-    providers::{Middleware, Provider},
+    providers::{Http, Middleware, Provider},
     types::Address,
     utils::L2_ETH_TOKEN_ADDRESS,
     ZKMiddleware,
@@ -15,8 +15,8 @@ use zksync_ethers_rs::{
 pub(crate) struct Args {
     #[clap(long = "token")]
     pub token_address: Option<Address>,
-    #[clap(long = "tol2", required = false)]
-    pub l2_token: bool,
+    #[clap(long = "l2", required = false)]
+    pub l2: bool,
     #[clap(long = "l1", required = false)]
     pub l1: bool,
 }
@@ -29,49 +29,146 @@ pub(crate) async fn run(args: Args, cfg: ZKSyncConfig) -> eyre::Result<()> {
     )?;
     let l2_provider = Provider::try_from(cfg.network.l2_rpc_url)?;
 
-    let provider = if args.l1 {
-        l1_provider.clone()
-    } else {
-        l2_provider.clone()
+    let wallet_address = cfg.wallet.context("Wallet config missing")?.address;
+
+    let network = match (args.l1, args.l2) {
+        (false, false) => "L2",
+        (false, true) => "L2",
+        (true, false) => "L1",
+        (true, true) => "BOTH",
     };
 
-    let wallet_address = cfg.wallet.context("Wallet config missing")?.address;
-    let network = if args.l1 { "L1" } else { "L2" };
+    let base_token_address = l2_provider.get_base_token_l1_address().await?;
 
-    if let Some(mut token_address) = args.token_address {
-        if args.l2_token && !args.l1 {
-            token_address = get_l2_token_from_l1_address(token_address, &l2_provider).await;
-        }
-        let erc20 = ERC20::new(token_address, Arc::new(provider));
-        let balance = erc20.balance_of(wallet_address).await?;
-        let token_symbol = erc20.symbol().await?;
-        let token_decimals: i32 = erc20.decimals().await?.into();
-        let parsed_balance = format_units(balance, token_decimals)?;
-        println!("[{network}] Balance: {parsed_balance} {token_symbol}");
-    } else {
-        match network {
-            "L1" => {
-                let balance = provider.get_balance(wallet_address, None).await?;
+    match network {
+        "L1" => {
+            if let Some(token_address) = args.token_address {
+                let (parsed_balance, _, token_symbol) = get_erc20_balance_decimals_symbol(
+                    token_address,
+                    wallet_address,
+                    l1_provider.clone(),
+                )
+                .await?;
+                println!("[{network}] Balance: {parsed_balance} {token_symbol}");
+            } else {
+                let balance = l1_provider.get_balance(wallet_address, None).await?;
                 let parsed_balance = format_ether(balance);
                 println!("[{network}] Balance: {parsed_balance} ETH");
             }
-            "L2" => {
-                let base_token_address = l2_provider.get_base_token_l1_address().await?;
-                println!("Base Token Address: {base_token_address}");
-                let balance = provider.get_balance(wallet_address, None).await?;
-                if base_token_address != L2_ETH_TOKEN_ADDRESS {
-                    let erc20 = ERC20::new(base_token_address, Arc::new(l1_provider));
-                    let token_symbol = erc20.symbol().await?;
-                    let token_decimals: i32 = erc20.decimals().await?.into();
-                    let parsed_balance = format_units(balance, token_decimals)?;
-                    println!("[{network}] Base Token Balance: {parsed_balance} {token_symbol}");
-                } else {
-                    let parsed_balance = format_ether(balance);
-                    println!("[{network}] Base Token Balance: {parsed_balance} ETH");
-                }
-            }
-            _ => unreachable!(),
         }
+        "L2" => {
+            println!("Base Token Address: {base_token_address:?}");
+            if let Some(token_address) = args.token_address {
+                if token_address == base_token_address {
+                    print_l2_base_token_balance(
+                        base_token_address,
+                        wallet_address,
+                        l2_provider.clone(),
+                        l1_provider.clone(),
+                    )
+                    .await?;
+                } else {
+                    let (parsed_balance, _, token_symbol) = get_erc20_balance_decimals_symbol(
+                        token_address,
+                        wallet_address,
+                        l2_provider.clone(),
+                    )
+                    .await?;
+                    println!("[{network}] Balance: {parsed_balance} {token_symbol}");
+                }
+            } else {
+                print_l2_base_token_balance(
+                    base_token_address,
+                    wallet_address,
+                    l2_provider.clone(),
+                    l1_provider.clone(),
+                )
+                .await?;
+            }
+        }
+        "BOTH" => {
+            println!("Base Token Address: {base_token_address:?}");
+            if let Some(token_address) = args.token_address {
+                // L2
+                let l2_token_address =
+                    get_l2_token_from_l1_address(token_address, &l2_provider).await;
+                if token_address == base_token_address {
+                    print_l2_base_token_balance(
+                        base_token_address,
+                        wallet_address,
+                        l2_provider.clone(),
+                        l1_provider.clone(),
+                    )
+                    .await?;
+                } else {
+                    let (parsed_balance, _, token_symbol) = get_erc20_balance_decimals_symbol(
+                        l2_token_address,
+                        wallet_address,
+                        l2_provider.clone(),
+                    )
+                    .await?;
+                    println!("[L2] Balance: {parsed_balance} {token_symbol}");
+                }
+                // L1
+                let (parsed_balance, _, token_symbol) = get_erc20_balance_decimals_symbol(
+                    token_address,
+                    wallet_address,
+                    l1_provider.clone(),
+                )
+                .await?;
+                println!("[L1] Balance: {parsed_balance} {token_symbol}");
+            } else {
+                // L2
+                print_l2_base_token_balance(
+                    base_token_address,
+                    wallet_address,
+                    l2_provider.clone(),
+                    l1_provider.clone(),
+                )
+                .await?;
+                // L1
+                let balance = l1_provider.get_balance(wallet_address, None).await?;
+                let parsed_balance = format_ether(balance);
+                println!("[L1] Balance: {parsed_balance} ETH");
+            }
+        }
+        _ => unreachable!(),
+    }
+    Ok(())
+}
+
+async fn get_erc20_balance_decimals_symbol(
+    token_address: Address,
+    wallet_address: Address,
+    provider: Provider<Http>,
+) -> eyre::Result<(String, i32, String)> {
+    let erc20 = ERC20::new(token_address, Arc::new(provider));
+    let balance = erc20.balance_of(wallet_address).await?;
+    let token_symbol = erc20.symbol().await?;
+    let token_decimals: i32 = erc20.decimals().await?.into();
+    let parsed_balance = format_units(balance, token_decimals)?;
+    Ok((parsed_balance, token_decimals, token_symbol))
+}
+
+async fn print_l2_base_token_balance(
+    base_token_address: Address,
+    wallet_address: Address,
+    l2_provider: Provider<Http>,
+    l1_provider: Provider<Http>,
+) -> eyre::Result<()> {
+    let balance = l2_provider.get_balance(wallet_address, None).await?;
+    if base_token_address != L2_ETH_TOKEN_ADDRESS {
+        let (_, token_decimals, token_symbol) = get_erc20_balance_decimals_symbol(
+            base_token_address,
+            wallet_address,
+            l1_provider.clone(),
+        )
+        .await?;
+        let parsed_balance = format_units(balance, token_decimals)?;
+        println!("[L2] Base Token Balance: {parsed_balance} {token_symbol}");
+    } else {
+        let parsed_balance = format_ether(balance);
+        println!("[L2] Base Token Balance: {parsed_balance} ETH");
     }
     Ok(())
 }
