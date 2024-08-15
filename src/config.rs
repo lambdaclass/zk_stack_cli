b683be233b005f2d8a63a2d25e281a5c06dbae1d
ilitteri <ilitteri@fi.uba.ar>
2024-08-15 15:35:16 -0300

    Try load config for every command but `config`

--- src/config.rs
+++ src/config.rs
@@ -1,14 +1,18 @@
+use crate::commands::config::{
+    common::{selected_config_path, SELECTED_CONFIG_FILE_NAME},
+    set,
+};
 use eyre::Context;
-use serde::Deserialize;
+use serde::{Deserialize, Serialize};
 use zksync_ethers_rs::types::Address;
 
-#[derive(Deserialize)]
+#[derive(Deserialize, Serialize)]
 pub struct ZKSyncConfig {
     pub network: NetworkConfig,
     pub wallet: Option<WalletConfig>,
 }
 
-#[derive(Deserialize)]
+#[derive(Deserialize, Serialize)]
 pub struct NetworkConfig {
     pub l1_rpc_url: Option<String>,
     pub l1_explorer_url: Option<String>,
@@ -16,17 +20,33 @@ pub struct NetworkConfig {
     pub l2_explorer_url: Option<String>,
 }
 
-#[derive(Deserialize)]
+#[derive(Deserialize, Serialize)]
 pub struct WalletConfig {
     pub address: Address,
     pub private_key: String,
 }
 
-pub fn config_path() -> String {
-    format!("{}/etc/config.toml", env!("CARGO_MANIFEST_DIR"))
+pub async fn try_load_selected_config() -> eyre::Result<Option<ZKSyncConfig>> {
+    let config_path = selected_config_path()?;
+    if !config_path.exists() {
+        return Ok(None);
+    }
+    let config = std::fs::read_to_string(config_path).context("Failed to read config file")?;
+    toml::from_str(&config)
+        .context("Failed to parse config file")
+        .map(Some)
 }
 
-pub fn load_config() -> eyre::Result<ZKSyncConfig> {
-    let config = std::fs::read_to_string(config_path()).context("Failed to read config file")?;
+pub async fn load_selected_config() -> eyre::Result<ZKSyncConfig> {
+    let config_path = selected_config_path()?;
+    if !config_path.exists() {
+        println!("No config set, please select a config to set");
+        set::run(set::Args {
+            config_name: SELECTED_CONFIG_FILE_NAME.into(),
+            set_config_interactively: true,
+        })
+        .await?;
+    }
+    let config = std::fs::read_to_string(config_path).context("Failed to read config file")?;
     toml::from_str(&config).context("Failed to parse config file")
 }
