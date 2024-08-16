80c28425eac9ace1e21881236feb8c9b3dfcb0e9
ilitteri <ilitteri@fi.uba.ar>
2024-08-16 19:22:55 -0300

    Fix propose-upgrade

--- src/commands/contracts/governance/propose.rs
+++ src/commands/contracts/governance/propose.rs
@@ -10,16 +10,16 @@ use zksync_ethers_rs::{
 
 #[derive(ClapArgs, PartialEq)]
 pub(crate) struct Args {
-    #[clap(short = 's', long, conflicts_with_all = &["transparent", "operation"])]
+    #[clap(short = 's', long, conflicts_with_all = &["transparent", "operation"], requires = "operation_id", required_unless_present = "transparent")]
     pub shadow: bool,
-    #[clap(long, conflicts_with_all = &["transparent", "operation"])]
-    pub operation_id: Hash,
-    #[clap(long, default_value = "0", value_parser = U256::from_dec_str)]
+    #[clap(long, conflicts_with_all = &["transparent", "operation"], requires = "shadow", required_unless_present = "transparent",)]
+    pub operation_id: Option<Hash>,
+    #[clap(long, default_value = "0", value_parser = U256::from_dec_str, required = false)]
     pub delay: U256,
-    #[clap(short = 't', long, conflicts_with_all = &["shadow", "operation_id"])]
+    #[clap(short = 't', long, conflicts_with_all = &["shadow", "operation_id"], requires = "operation", required_unless_present = "shadow")]
     pub transparent: bool,
-    #[clap(long, conflicts_with_all = &["shadow", "operation_id"], value_parser = parse_operation)]
-    pub operation: Operation,
+    #[clap(long, conflicts_with_all = &["shadow", "operation_id"], value_parser = parse_operation, requires = "transparent", required = false)]
+    pub operation: Option<Operation>,
     #[clap(long, required = false)]
     pub explorer_url: bool,
 }
@@ -31,14 +31,23 @@ pub(crate) async fn run(
 ) -> eyre::Result<()> {
     let transaction_receipt = if args.shadow {
         governance
-            .schedule_shadow(args.operation_id.into(), args.delay)
+            .schedule_shadow(
+                args.operation_id
+                    .context("--operation-id is required with --shadow")?
+                    .into(),
+                args.delay,
+            )
             .send()
             .await?
             .await?
             .context("No transaction receipt for shadow operation")?
     } else if args.transparent {
         governance
-            .schedule_transparent(args.operation, args.delay)
+            .schedule_transparent(
+                args.operation
+                    .context("--operation is required with --transparent")?,
+                args.delay,
+            )
             .send()
             .await?
             .await?
