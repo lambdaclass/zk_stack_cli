b33978ef5a0e548aa662d206bb897557a952b4df
ilitteri <ilitteri@fi.uba.ar>
2024-08-16 13:24:41 -0300

    Fix try_governance_from_config

--- src/commands/utils/mod.rs
+++ src/commands/utils/mod.rs
@@ -1,5 +1,6 @@
-use crate::config::ZKSyncConfig;
 use std::sync::Arc;
+
+use eyre::ContextCompat;
 use zksync_ethers_rs::{
     contracts::governance::Governance,
     middleware::SignerMiddleware,
@@ -7,22 +8,34 @@ use zksync_ethers_rs::{
     signers::{LocalWallet, Signer},
 };
 
+use crate::config::ZKSyncConfig;
+
 pub(crate) mod balance;
 
-pub(crate) fn try_l2_provider_from_config(cfg: &ZKSyncConfig) -> eyre::Result<Provider<Http>> {
-    Provider::try_from(cfg.network.l2_rpc_url.as_str()).map_err(Into::into)
+// pub(crate) fn try_l2_provider_from_config(cfg: &ZKSyncConfig) -> eyre::Result<Provider<Http>> {
+//     Provider::try_from(cfg.network.l2_rpc_url.as_str()).map_err(Into::into)
+// }
+
+pub(crate) fn try_l1_provider_from_config(cfg: &ZKSyncConfig) -> eyre::Result<Provider<Http>> {
+    Provider::try_from(
+        cfg.network
+            .l1_rpc_url
+            .as_deref()
+            .context("L1 RPC URL not found in config")?,
+    )
+    .map_err(Into::into)
 }
 
 pub(crate) async fn try_governance_from_config(
     cfg: &ZKSyncConfig,
 ) -> eyre::Result<Governance<SignerMiddleware<impl Middleware, impl Signer>>> {
-    let l2_provider = try_l2_provider_from_config(&cfg)?;
-    let l2_chain_id = l2_provider.get_chainid().await?.as_u64();
+    let l1_provider = try_l1_provider_from_config(&cfg)?;
+    let l1_chain_id = l1_provider.get_chainid().await?.as_u64();
     let wallet = cfg
         .governance
         .owner_private_key
         .parse::<LocalWallet>()?
-        .with_chain_id(l2_chain_id);
-    let l2_signer = SignerMiddleware::new(l2_provider, wallet);
-    Ok(Governance::new(cfg.governance.address, Arc::new(l2_signer)))
+        .with_chain_id(l1_chain_id);
+    let l1_signer = SignerMiddleware::new(l1_provider, wallet);
+    Ok(Governance::new(cfg.governance.address, Arc::new(l1_signer)))
 }
