c6edceb9d186a5dc859f0a351aa316d5443cbfc6
fborello-lambda <federicoborello@lambdaclass.com>
2024-08-23 19:11:30 -0300

    refactor(gas-scenario cmd): change structure based on new flags

--- src/commands/test.rs
+++ src/commands/test.rs
@@ -14,6 +14,7 @@ use std::{
     sync::Arc,
     thread::sleep,
 };
+use tokio::time::Instant;
 use zksync_ethers_rs::{
     core::utils::{format_ether, parse_ether},
     providers::Middleware,
@@ -60,8 +61,8 @@ pub(crate) enum Command {
         visible_alias = "gs"
     )]
     GasScenario {
-        #[clap(long = "wallets", short = 'w', required = true)]
-        number_of_wallets: u16,
+        #[clap(long = "tps", required = true)]
+        tps: u64,
         #[clap(
             long = "amount",
             short = 'a',
@@ -69,6 +70,13 @@ pub(crate) enum Command {
             help = "Amount of BaseToken to deposit, 20% more will be deposited.\nThat extra 20% will remain in the main wallet,\nthe rest will be redistributed to random wallets"
         )]
         amount: f32,
+        #[arg(
+            long = "minutes",
+            short = 'm',
+            default_value_t = 1,
+            help = "Minutes to run the program in a loop"
+        )]
+        minutes: u64,
     },
 }
 
@@ -259,9 +267,32 @@ impl Command {
                 Ok(())
             }
             Command::GasScenario {
-                number_of_wallets,
+                tps,
                 amount,
+                minutes,
             } => {
+                // TPS, at the moment the calculation is performed with the following corollaries
+                // - Don't take deposits into account
+                // - 1 transaction from the rich wallet to each random wallet
+                // - 1 transaction from each random wallet to the rich wallet
+                // - sleep 1 second and start again. This step is made to simulate the number of transaction per second
+                //  - (disclaimer) this tps is not the same tps of the chain, here we are sending transactions, the chain is processing transactions
+                //  - (disclaimer) moreover, the "downtime" of the deposit transaction is not taken into account. This reduces the actual tps.
+                //  - the only variable is the amount of randowm wallets
+                //  - taking into account we will have 2*number_of_wallets transactions
+                //  - the number_of_wallets is calculated as follows: 2*number_of_wallets [txs] = tps [tx/s] * 1 [s]
+                //  - so number_of_wallets = (tps+1)/2. The +1 is to have a round number. 3/2 = 1 but 4/2 = 2
+
+                let number_of_wallets = ((tps + 1) / 2).try_into()?;
+
+                let mut fees: Vec<U256> = Vec::new();
+                let mut gas: Vec<U256> = Vec::new();
+                let mut gas_price: Vec<U256> = Vec::new();
+
+                let mut fees_per_run = U256::zero();
+                let mut gas_per_run = U256::zero();
+                let mut gas_price_per_run = U256::zero();
+
                 let wallets =
                     get_n_random_wallets(number_of_wallets, &l1_provider, &l2_provider).await?;
                 // ideally it should be the amount transferred, the gas + fees have to be deducted automatically
@@ -289,116 +320,124 @@ impl Command {
                 )
                 .await?;
 
-                if l2_balance.le(&parsed_amount_to_deposit) {
-                    spinner.update(spinners::Dots, "Checking L1 Balance", Color::Blue);
+                let start_time = Instant::now();
+                while start_time.elapsed() <= tokio::time::Duration::from_secs(minutes * 10) {
+                    if l2_balance.le(&parsed_amount_to_deposit) {
+                        spinner.update(spinners::Dots, "Checking L1 Balance", Color::Blue);
 
-                    // Here we are assuming that the base token has 18 decimals
-                    if parse_ether(l1_balance)?.le(&parsed_amount_to_deposit) {
-                        let mint_amount = parsed_amount_to_deposit;
+                        // Here we are assuming that the base token has 18 decimals
+                        if parse_ether(&l1_balance)?.le(&parsed_amount_to_deposit) {
+                            let mint_amount = parsed_amount_to_deposit;
 
-                        let msg = format!(
-                            "Not enough tokens... Minting {} {token_symbol}",
-                            format_ether(mint_amount)
-                        );
-                        spinner.update(spinners::Dots, msg, Color::Blue);
+                            let msg = format!(
+                                "Not enough tokens... Minting {} {token_symbol}",
+                                format_ether(mint_amount)
+                            );
+                            spinner.update(spinners::Dots, msg, Color::Blue);
 
-                        let future_receipt =
-                            erc20_l1_mint(base_token_address, &arc_zk_wallet, mint_amount);
+                            let future_receipt =
+                                erc20_l1_mint(base_token_address, &arc_zk_wallet, mint_amount);
 
-                        let receipt = future_receipt.await?;
+                            let receipt = future_receipt.await?;
 
-                        display_balance(Some(base_token_address), &arc_zk_wallet, true, false)
-                            .await?;
-                        println!("Transaction Hash: {:?}", receipt.transaction_hash);
+                            display_balance(Some(base_token_address), &arc_zk_wallet, true, false)
+                                .await?;
+                            println!("Transaction Hash: {:?}", receipt.transaction_hash);
+                        }
+                        spinner.update(spinners::Dots, "Depositing", Color::Blue);
+                        // Begin Deposit from rich wallet to rich wallet
+                        deposit_base_token(&arc_zk_wallet, parsed_amount_to_deposit).await?;
+                        // End Deposit from rich wallet to rich wallet
+                        spinner.success("Success, Deposit");
+                    } else {
+                        spinner.success("Enough L2 balance");
                     }
-                    spinner.update(spinners::Dots, "Depositing", Color::Blue);
-                    // Begin Deposit from rich wallet to rich wallet
-                    deposit_base_token(&arc_zk_wallet, parsed_amount_to_deposit).await?;
-                    // End Deposit from rich wallet to rich wallet
-                    spinner.success("Success, Deposit");
-                } else {
-                    spinner.success("Enough L2 balance");
-                }
 
-                // Begin Transfer from rich wallet to each wallet
+                    // Begin Transfer from rich wallet to each wallet
 
-                println!(
-                    "{} Transfer from {} wallet to {} wallet.",
-                    "[L2->L2]".bold().bright_cyan().on_black(),
-                    "rich".bold().red().on_black(),
-                    "each".bold().blue().on_black()
-                );
+                    println!(
+                        "{} Transfer from {} wallet to {} wallet.",
+                        "[L2->L2]".bold().bright_cyan().on_black(),
+                        "rich".bold().red().on_black(),
+                        "each".bold().blue().on_black()
+                    );
 
-                println!(
-                    "{}",
-                    "Waiting for all transactions to finish".yellow().on_black()
-                );
+                    println!(
+                        "{}",
+                        "Waiting for all transactions to finish".yellow().on_black()
+                    );
 
-                let tx_hashes_forwards = send_transactions(
-                    &arc_zk_wallet,
-                    &wallets,
-                    parsed_amount_of_bt_to_transfer_for_each,
-                )
-                .await?;
+                    let tx_hashes_forwards = send_transactions(
+                        &arc_zk_wallet,
+                        &wallets,
+                        parsed_amount_of_bt_to_transfer_for_each,
+                    )
+                    .await?;
 
-                display_balances(&wallets).await?;
+                    display_balances(&wallets).await?;
 
-                // End Transfer from rich wallet to each wallet
-                println!("{}", "#".repeat(64));
-                // Begin Transfer from each wallet to rich wallet
+                    // End Transfer from rich wallet to each wallet
+                    println!("{}", "#".repeat(64));
+                    // Begin Transfer from each wallet to rich wallet
 
-                display_balance(None, &arc_zk_wallet, false, true).await?;
+                    display_balance(None, &arc_zk_wallet, false, true).await?;
 
-                println!(
-                    "{} Transfer from {} wallet to {} wallet.",
-                    "[L1->L2]".bold().bright_cyan().on_black(),
-                    "each".bold().blue().on_black(),
-                    "rich".bold().red().on_black()
-                );
+                    println!(
+                        "{} Transfer from {} wallet to {} wallet.",
+                        "[L1->L2]".bold().bright_cyan().on_black(),
+                        "each".bold().blue().on_black(),
+                        "rich".bold().red().on_black()
+                    );
 
-                let _tx_hashes_backwards = send_transactions_back(&wallets, &arc_zk_wallet).await?;
+                    let _tx_hashes_backwards =
+                        send_transactions_back(&wallets, &arc_zk_wallet).await?;
 
-                // End Transfer from each wallet to rich wallet
-                println!("{}", "#".repeat(64));
-                let mut fees = U256::zero();
-                let mut gas = U256::zero();
-                let mut gas_price = U256::zero();
-                for h in tx_hashes_forwards {
-                    let receipt = l2_provider
-                        .get_transaction_receipt(h)
-                        .await?
-                        .context("Error unwrapping tx_receipt")?;
+                    // End Transfer from each wallet to rich wallet
+                    println!("{}", "#".repeat(64));
 
-                    let gas_used = receipt.gas_used.context("Error unwrapping gas_used")?;
-                    let receipt_gas_price = receipt
-                        .effective_gas_price
-                        .context("Error unwrapping gas price")?;
+                    for h in tx_hashes_forwards {
+                        let receipt = l2_provider
+                            .get_transaction_receipt(h)
+                            .await?
+                            .context("Error unwrapping tx_receipt")?;
 
-                    let details = l2_provider
-                        .get_transaction_details(h)
-                        .await?
-                        .context("Error unwrapping tx_details")?;
+                        let gas_used = receipt.gas_used.context("Error unwrapping gas_used")?;
+                        let receipt_gas_price = receipt
+                            .effective_gas_price
+                            .context("Error unwrapping gas price")?;
 
-                    fees = fees.add(details.fee);
-                    if gas_price.is_zero() {
-                        gas_price = receipt_gas_price;
+                        let details = l2_provider
+                            .get_transaction_details(h)
+                            .await?
+                            .context("Error unwrapping tx_details")?;
+
+                        fees_per_run = fees_per_run.add(details.fee);
+                        if gas_price_per_run.is_zero() {
+                            gas_price_per_run = receipt_gas_price;
+                        }
+                        gas_price_per_run = (receipt_gas_price + gas_price_per_run) / 2_u32;
+                        gas_per_run = gas_per_run.add(gas_used);
                     }
-                    gas_price = (receipt_gas_price + gas_price) / 2_u32;
-                    gas = gas.add(gas_used);
-                }
 
-                println!(
-                    "{} Transaction(s)\nGas Used: {}\nTotal Fees: {}",
-                    format!("{}", wallets.len())
-                        .bold()
-                        .bright_yellow()
-                        .on_black(),
-                    format!("{gas}").bold().bright_green().on_black(),
-                    format!("{} {}", format_ether(fees), token_symbol)
-                        .bold()
-                        .bright_cyan()
-                        .on_black(),
-                );
+                    println!(
+                        "{} Transaction(s)\nGas Used: {}\nTotal Fees: {}",
+                        format!("{}", wallets.len())
+                            .bold()
+                            .bright_yellow()
+                            .on_black(),
+                        format!("{gas_price_per_run}")
+                            .bold()
+                            .bright_green()
+                            .on_black(),
+                        format!("{} {}", format_ether(fees_per_run), token_symbol)
+                            .bold()
+                            .bright_cyan()
+                            .on_black(),
+                    );
+                    fees.push(fees_per_run);
+                    gas.push(gas_per_run);
+                    gas_price.push(gas_price_per_run);
+                }
 
                 Ok(())
             }
