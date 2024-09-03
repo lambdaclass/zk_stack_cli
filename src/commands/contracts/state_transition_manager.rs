13d5804f53317c6248fd21b418bc36edbbfca455
Manuel Bilbao <manuel.bilbao@lambdaclass.com>
2024-09-03 12:23:48 -0300

    Add SetPriorityTxMaxGasLimit

--- src/commands/contracts/state_transition_manager.rs
+++ src/commands/contracts/state_transition_manager.rs
@@ -10,19 +10,37 @@ use super::governance::run_upgrade;
 
 #[derive(Subcommand)]
 pub(crate) enum Command {
+    #[command(name = "freeze", about = "Freeze chain", visible_alias = "fr")]
     FreezeChain {
         #[clap(index = 1, required = true)]
         chain_id: U256,
     },
+    #[command(name = "unfreeze", about = "Unfreeze chain", visible_alias = "uf")]
     UnfreezeChain {
         #[clap(index = 1, required = true)]
         chain_id: U256,
     },
+    #[command(
+        name = "register-deployed-hyperchain",
+        about = "Register already deployed hyperchain",
+        visible_alias = "rdh"
+    )]
     RegisterAlreadyDeployedHyperchain {
         #[clap(index = 1, required = true)]
         chain_id: U256,
         #[clap(index = 2, required = true)]
         hyperchain_address: Address,
+    },
+    #[command(
+        name = "priority-gas-limit",
+        about = "Set priority tx max gas limit",
+        visible_alias = "pgl"
+    )]
+    SetPriorityTxMaxGasLimit {
+        #[clap(index = 1, required = true)]
+        chain_id: U256,
+        #[clap(index = 2, required = true)]
+        max_gas_limit: U256,
     },
 }
 
@@ -32,12 +50,12 @@ impl Command {
         let state_transition_manager = try_state_transition_manager_from_config(&cfg).await?;
         match self {
             Command::FreezeChain { chain_id } => {
-                let freeze_calldata = state_transition_manager
+                let calldata = state_transition_manager
                     .freeze_chain(chain_id)
                     .function
                     .encode_input(&chain_id.into_tokens())?;
                 run_upgrade(
-                    freeze_calldata.into(),
+                    calldata.into(),
                     false,
                     true,
                     0.into(),
@@ -48,12 +66,12 @@ impl Command {
                 .await?;
             }
             Command::UnfreezeChain { chain_id } => {
-                let unfreeze_calldata = state_transition_manager
+                let calldata = state_transition_manager
                     .unfreeze_chain(chain_id)
                     .function
                     .encode_input(&chain_id.into_tokens())?;
                 run_upgrade(
-                    unfreeze_calldata.into(),
+                    calldata.into(),
                     false,
                     true,
                     0.into(),
@@ -67,14 +85,35 @@ impl Command {
                 chain_id,
                 hyperchain_address,
             } => {
-                let register_hyperchain_calldata = state_transition_manager
+                let calldata = state_transition_manager
                     .register_already_deployed_hyperchain(chain_id, hyperchain_address)
                     .function
                     .encode_input(
                         &[chain_id.into_tokens(), hyperchain_address.into_tokens()].concat(),
                     )?;
                 run_upgrade(
-                    register_hyperchain_calldata.into(),
+                    calldata.into(),
+                    false,
+                    true,
+                    0.into(),
+                    false,
+                    governance,
+                    cfg,
+                )
+                .await?;
+            }
+            Command::SetPriorityTxMaxGasLimit {
+                chain_id,
+                max_gas_limit,
+            } => {
+                let calldata = state_transition_manager
+                    .set_priority_tx_max_gas_limit(chain_id, max_gas_limit)
+                    .function
+                    .encode_input(
+                        &[chain_id.into_tokens(), max_gas_limit.into_tokens()].concat(),
+                    )?;
+                run_upgrade(
+                    calldata.into(),
                     false,
                     true,
                     0.into(),
